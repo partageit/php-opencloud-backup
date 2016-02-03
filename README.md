0147347824deaa2321c5de1c995a6ed91efb51b6
David Duquenoy <david@partage-it.com>
2016-02-03 11:24:05 +0100

    Script parameters and JSON configuration file
    Script parameters are now checked and are more pretty
    Credentials and URIs are now provided from a JSON file

--- README.md
+++ README.md
@@ -10,12 +10,43 @@ Thank you [BaptisteDixneuf](http://baptistedixneuf.fr/) :)
 
 ## Installation
 
+git and [composer](https://getcomposer.org/) must be present.
+
+    git clone https://github.com/partageit/php-opencloud-backup.git
+    composer update
+
 ## Usage
 
+To start backup of `/home/me/photos/*.jpeg` in the container `photos-2016` reading credentials from `config.json`:
+
+    php oc-backup.php --files=/home/me/photos/*.jpeg --container=photos-2016 --config=config.json
+
+For further details, type:
+
+    php oc-backup.php --help
+
+### Configuration file
+
+```json
+{
+	"authUrl": "https://auth.cloud.ovh.net/v2.0",
+	"username": "",
+	"password": "",
+	"tenant": "",
+	"swiftUrl": "",
+	"serviceName": "swift",
+	"region": ""
+}
+```
+
+Where: 
+
+- authUrl: Access point to the Identity service, defined in "Accès et sécurité/Accès API"
+- username: Horizon user
+- password: Horizon password
+- tenant: OS_TENANT_NAME value from the RC OpenStack file
+- swiftUrl: Access point to the Object store service, defined in "Accès et sécurité/Accès API"
+- serviceName: Always swift?
+- region: the region name, in GRA1, SBG1, ...
+
 ## [License](LICENSE)
-
-## Todo
-
-- handle errors
-- better argv handling
-- display help message
---
+++ oc-backup.php
@@ -0,0 +1,86 @@
+<?php
+
+require "vendor/autoload.php";
+
+use OpenCloud\OpenStack;
+
+$longOptions = array(
+	"help",
+	"files:",
+	"container:",
+	"config:"
+);
+$parameters = getopt("h", $longOptions);
+
+if (isset($parameters["help"]) || isset($parameters["h"])) {
+	usage();
+	exit(0);
+}
+
+if (count($parameters) !== 3) {
+	echo "Required parameters are missing\n\n";
+	usage();
+	exit(1);
+}
+
+$files = $parameters["files"];
+$containerName = $parameters["container"];
+$configFile = $parameters["config"];
+
+if (!file_exists($configFile)) {
+	echo "The configuration file '$configFile' does not exist.\n\n";
+	exit(2);
+}
+
+$ocParameters = json_decode(file_get_contents($configFile));
+
+if (!$ocParameters) {
+	echo "The configuration file '$configFile' content is not valid JSON.\n\n";
+	exit(3);
+}
+
+// Initialization
+$client = new OpenStack(
+	$ocParameters->authUrl, array(
+		"username"=> $ocParameters->username,
+		"password"=> $ocParameters->password,
+		"tenantName"  => $ocParameters->tenant
+	)
+);
+
+$swiftUrl = $ocParameters->swiftUrl;
+$serviceName = $ocParameters->serviceName;
+$region = $ocParameters->region;
+
+try {
+
+	$client->authenticate();
+	$service = $client->objectStoreService($serviceName, $region);
+	$container = $service->createContainer($containerName);
+
+	foreach (glob($files) as $filename) {
+		echo "Sending $filename (" . number_format(filesize($filename) / 1024 / 1024, 2) . " MB)\n";
+		$fileData = fopen($filename, "r+");
+		$container->uploadObject(basename($filename), $fileData);
+	}
+
+} catch (Exception $e) {
+	echo "Problem while dealing with OpenCloud:\n";
+	echo $e->getMessage() . "\n";
+	exit(4);
+}
+
+function usage() {
+	global $argv;
+	echo basename($argv[0]) . "\n\n";
+	echo "Backup files to an OpenCloud container\n\n";
+	echo "Usage: " . basename($argv[0]) . " --files=*.jpeg --container=mycontainer --config=config.json\n\n";
+	echo "Parameters:\n";
+	echo " --help      : Displays this message.\n";
+	echo " --files     : Files to backup, wildcards are allowed.\n";
+	echo " --container : The container name, automatically created if it does not exist.\n";
+	echo " --config    : Path to the configuration file.\n";
+	echo "\n";
+}
+
+?>
